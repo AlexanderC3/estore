02afb6a18db0bb0a61e2086f133b6349421e34ab
AlexandeC3U <79148472+AlexandeC3U@users.noreply.github.com>
2023-12-03 15:58:54 +0100

    install and setup trpc, add auth api

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -13,6 +13,7 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from "@/lib/validators/account-credentials-validator";
+import { trpc } from "@/trpc/client";
 
 const Page = () => {
   const {
@@ -23,8 +24,10 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   });
 
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
-    // TODO: send data to the server
+    mutate({ email, password });
   };
 
   return (
@@ -54,6 +57,7 @@ const Page = () => {
                   <Label htmlFor="email">Email</Label>
                   <Input
                     {...register("email")}
+                    type="email"
                     className={cn({
                       "focus-visible:ring-red-500": errors.email,
                     })}
@@ -64,6 +68,7 @@ const Page = () => {
                   <Label htmlFor="password">Password</Label>
                   <Input
                     {...register("password")}
+                    type="password"
                     className={cn({
                       "focus-visible:ring-red-500": errors.password,
                     })}
